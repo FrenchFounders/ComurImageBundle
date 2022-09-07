55e4e9258be39661b6e8e8cdfe0eacf8a6d6e926
Jonathan Martin <jmartin@iw2.fr>
2022-09-07 14:49:12 +0200

    Fix deprecated

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('comur_image');
+        $treeBuilder = new TreeBuilder('comur_image');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
