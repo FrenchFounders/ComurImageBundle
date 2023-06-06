8e6cd3b4820fbabf8cd9b31d03a457e480f02981
yespana <yannick@frenchfounders.com>
2023-06-06 12:14:11 +0200

    Update FormPass.php

--- DependencyInjection/Compiler/FormPass.php
+++ DependencyInjection/Compiler/FormPass.php
@@ -24,36 +24,5 @@ class FormPass implements CompilerPassInterface
     /**
      * {@inheritdoc}
      */
-    public function process(ContainerBuilder $container)
-    {
-        $template = "ComurImageBundle:Form:fields.html.twig";
-        $resources = $container->getParameter('twig.form.resources');
-        // Ensure it wasn't already added via config
-        if (!in_array($template, $resources)) {
-            // If fields.html.twig is found, insert ComurImageBundle right after
-            // Else insert ComurImageBundle in first position
-            if (false !== ($key = array_search('fields.html.twig', $resources))) {
-                array_splice($resources, ++$key, 0, $template);
-            } else {
-                array_unshift($resources, $template);
-            }
-
-            $container->setParameter('twig.form.resources', $resources);
-        }
-
-        $template = "ComurImageBundle:Form:croppable_image_modal.html.twig";
-        // Ensure it wasn't already added via config
-        if (!in_array($template, $resources)) {
-            // If form_div_layout.html.twig is found, insert ComurImageBundle right after
-            // Else insert ComurImageBundle in first position
-            if (false !== ($key = array_search('form_div_layout.html.twig', $resources))) {
-                array_splice($resources, ++$key, 0, $template);
-            } else {
-                array_unshift($resources, $template);
-            }
-
-            $container->setParameter('twig.form.resources', $resources);
-        }
-        // var_dump($resources);exit;
-    }
+    public function process(ContainerBuilder $container) {}
 }
